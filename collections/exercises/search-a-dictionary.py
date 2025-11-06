d262b69877d155fec8197f59af723a27a4469fd9
Xaxziminrax <p.yadrich@gmail.com>
2025-11-06 17:03:04 -0600

    man that was a lot of reading

--- collections/exercises/search-a-dictionary.py
+++ collections/exercises/search-a-dictionary.py
@@ -11,22 +11,42 @@ flavors = {
 
 ## Set a variable called choice to the flavor you want to search for.
 
+choice = 'vanilla'
+
 ## Use an if statement to check if choice is in the flavors dictionary.
 
+if('vanilla' in flavors.keys()):
+  print("bless")
+
 ## If it is, set another variable called cost to the value associated with choice.
+
+cost = flavors['vanilla']
+
 
 ## If it isn’t, set cost to 0.
 
 ## Print the cost.
 
+print(cost)
+
 ### Search a Dictionary Part 2:
 
 ## Initialize two variables: highest_cost to 0 and fanciest to an empty string.
-
+highest_cost = 0
+fanciest = ''
 ## Loop through the flavors dictionary using a for loop.
 
+
 ## For each flavor, check if its price is higher than highest_cost.
+
 
 ## If it is, update fanciest to this flavor and highest_cost to its price.
 
 ## After the loop, print the most expensive flavor.
+
+for flavor in flavors.keys():
+  # print(flavor, flavors[flavor])
+  if(flavors[flavor]) > highest_cost:
+    highest_cost = flavors[flavor]
+    fanciest = flavor
+print(fanciest, highest_cost)
--- collections/exercises/strings.py
+++ collections/exercises/strings.py
@@ -5,21 +5,57 @@ word = 'tomato'
 
 # 1. Print a slice of the first 12 characters from 'text'.
 
+textLen = int(len(text))
+textStartSlice = text[0:12]
+#print(len(textSlice))
+print(textStartSlice)
 
 # 2. Print a slice of the last 12 characters from 'text'. You should NOT have to count the index values yourself!
 
+endTextSlice = text[(textLen-12):(textLen)]
+#print(len(endTextSlice))
+print(endTextSlice)
 
 # 3. Print a slice of the middle 12 characters from 'text'.
 
+textMidStart = int(textLen/3)
+textMidEnd = int(textLen/3*2)
+textMiddle = text[textMidStart:textMidEnd]
+#print(len(textMiddle))
+print(textMiddle)
 
 # ---- Exercise 3: Looping Through a String ----
 
 # Use index values to loop backwards through 'word'.
 
 # 1. Print 1 letter per line.
+loopCount = 0
+for index in range(len(word)):
+    wordIndex = int(len(word))
+    loopCount += 1
+    print(word[int(wordIndex-loopCount)])
 
+    # NOTE: I am aware this is an inefficient way to do it, but got the idea and wanted to see if I could make it possible, and I did that
 
 # 2. Refactor the code to use the accumulator pattern to build up and print the reversed string. For example, if given 'good', print 'doog' on one line.
+loopCount = 0
+accumulator = ''
+for index in range(len(word)):
+    wordIndex = int(len(word))
+    loopCount += 1
+    accumulator = accumulator + word[int(wordIndex-loopCount)]
+    # print(word[int(wordIndex-loopCount)])
 
-
+print(accumulator)
 # 3. Refactor the code to print a combination of the original and reversed string. For example, given 'tomato', print 'tomatootamot'. (If you want to be fancy, print 'tomato | otamot').
+loopCount = 0
+accumulator = ''
+rotalumucca = ''
+for index in range(len(word)):
+    wordIndex = int(len(word))
+    loopCount += 1
+    accumulator = accumulator + word[int(wordIndex-loopCount)]
+    rotalumucca = rotalumucca + word[index]
+    # print(word[int(wordIndex-loopCount)])
+
+print(rotalumucca, '|', accumulator)
